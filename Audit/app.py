2b59aff0bb6d8e51f7e8e0beb65eec7070ca4e1a
erjinwang <erjin1994@gmail.com>
2021-04-13 14:34:35 -0700

    lab9

--- Audit/app.py
+++ Audit/app.py
@@ -89,6 +89,8 @@ def get_delivery_request_reading(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(port=8110)
---
+++ dashboard-ui/src/components/AppStats.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AppStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`http://acit3855-erjin.eastus2.cloudapp.azure.com:8100/stats`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+							<th>Order Request</th>
+							<th>Delivery Request</th>
+						</tr>
+						<tr>
+							<td># OR: {stats['order_req_received_since_last_update']}</td>
+							<td># DR: {stats['delivery_req_received_since_last_update']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Total Number of OR Today: {stats['total_order_req_today']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">Total Number of DR Today: {stats['otal_delivery_req_toda']}</td>
+						</tr>
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+
+            </div>
+        )
+    }
+}
